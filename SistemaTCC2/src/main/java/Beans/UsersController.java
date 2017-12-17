f64a0f6b56fd75ff25903c9360a5bd5cb19aaa3b
bernafan <bfmello@hotmail.com>
2017-12-17 18:13:26 -0200

    Login de usuarios

---
+++ SistemaTCC2/src/main/java/Beans/UsersController.java
@@ -0,0 +1,148 @@
+package Beans;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import DAO.UsersEJB;
+import Models.Users;
+
+@ManagedBean(name="users")
+@RequestScoped
+public class UsersController {
+    FacesContext context = FacesContext.getCurrentInstance();
+        
+    private Users user;
+    
+    @EJB
+    UsersEJB userEJB;
+    
+    private List<Users> userList = new ArrayList();
+    
+    public String LogIn() {
+    		String type = null;
+    		type  = userEJB.autenticar(user.getUserName(), user.getPassword());
+    		if(type != null) {
+    			if(type.equals("ADM")) {
+    				return "administrador";
+    			}else if (type.equals("STD")) {
+    				return "aluno";
+    			}else if (type.equals("PFS")) {
+    				return "professor";
+    			}else if(type.equals("INVALIDO")) {
+    			FacesMessage fm = new FacesMessage("Login ou senha inválidos");
+    			FacesContext.getCurrentInstance().addMessage("msg", fm);
+    			}
+    		}
+    		
+    		return "usuário inexistente";
+    }
+    
+
+    public String addUser(){
+        try
+        {
+            if(!user.getEmail().contains("@") || !user.getEmail().contains("."))
+            {
+                context.addMessage(null, new FacesMessage("Geçerli e-Mail değil"));
+                return null;
+            }
+                
+            
+            user = userEJB.saveUser(user);
+            
+            userList = userEJB.allUsers();
+            
+            context.addMessage(null, new FacesMessage("Kullanıcı Başarı ile Eklendi..."));
+            return "index.xhtml?faces-redirect=true";
+        }
+        catch(Exception e)
+        {
+            context.addMessage(null, new FacesMessage("Kullanıcı Eklenirken Hata Oluştu... \n "+e));
+            return null;
+        }
+    }
+    
+    @PostConstruct
+    public void getAllUsersList(){
+        user = new Users();
+        userList = userEJB.allUsers();
+        
+    }
+    public String editUsers(Users user){
+        this.user = user; 
+        return "edit.xhtml";
+    }
+    public String editUser(){
+                System.out.println("--------------------1--------------------------------");
+        System.out.println("--------------------1--------------------------------");
+        System.out.println(this.user.getId());
+        System.out.println("--------------------1--------------------------------");
+        System.out.println("--------------------1--------------------------------");
+
+        
+        if(!this.user.getEmail().contains("@") || !this.user.getEmail().contains("."))
+        {
+            context.addMessage(null, new FacesMessage("Invalid email"));
+            return null;
+        }
+
+        try
+        {
+        System.out.println("--------------------1--------------------------------");
+        System.out.println("---------------------2-------------------------------");
+            userEJB.editUsers(this.user);
+            
+        System.out.println("--------------------3--------------------------------");
+            userList = userEJB.allUsers();
+            
+        System.out.println("---------------------4-------------------------------");
+            return "index.xhtml?faces-redirect=true";
+        }
+        catch(Exception e)
+        {
+            context.addMessage(null, new FacesMessage("Kullanıcı Güncellerken Hata Oluştu... \n "+e));
+            return null;
+        }
+        
+    }
+    public String deleteUser(Users getUser){
+        try
+        {
+            userEJB.deleteUsers(getUser);
+            userList = userEJB.allUsers();
+            
+            context.addMessage(null, new FacesMessage("Kullanıcı Başarı ile Silindi..."));
+            return "index.xhtml?faces-redirect=true";
+        }
+        catch(Exception e)
+        {
+            context.addMessage(null, new FacesMessage("Kullanıcı Silinirken Hata Oluştu... \n "+e));
+            return null;
+        }
+    }
+    
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public List<Users> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<Users> userList) {
+        this.userList = userList;
+    }
+    
+}
